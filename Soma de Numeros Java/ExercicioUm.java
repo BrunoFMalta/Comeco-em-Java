d398dc6d46a7093a7dff09d75ad4876d21bd9f17
Bruno Felipe Malta <brunofmalta12@gmail.com>
2026-04-10 10:20:32 -0300

    Add files via upload

---
+++ Soma de Numeros Java/ExercicioUm.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class ExercicioUm {
+	
+	
+	public static void main (String [] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		
+		int x;
+		int y;
+		int resultado;
+		
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		resultado = x + y;
+		
+		System.out.println("SOMA =  " + resultado);
+		
+	
+
+	
+		sc.close();
+}
+
+	
+
+}
