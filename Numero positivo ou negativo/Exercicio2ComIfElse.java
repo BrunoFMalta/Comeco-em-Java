a31326f691dfb3fd0c8b47e33d2f5df834a0eefd
Bruno Felipe Malta <brunofmalta12@gmail.com>
2026-04-23 21:03:05 -0300

    Número positivo ou negativo
    Código que identifica se o número é negativo ou positivo!

---
+++ Numero positivo ou negativo/Exercicio2ComIfElse.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Exercicio2ComIfElse {
+
+	
+	public static void main (String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		int numero;
+		
+		System.out.println("digite um numero: ");
+		numero = sc.nextInt();
+		
+		if (numero < 0) {
+			System.out.println("NEGATIVO");
+		}else {
+			System.out.println("NAO NEGATIVO");
+			
+		}
+					
+sc.close();
+
+
+	}
+	
+}
