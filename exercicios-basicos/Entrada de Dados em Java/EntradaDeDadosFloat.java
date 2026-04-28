0531e22cb4e93434740917344b36e9d381b7776c
Bruno Felipe Malta <brunofmalta12@gmail.com>
2026-04-28 10:31:35 -0300

    Add files via upload

---
+++ exercicios-basicos/Entrada de Dados em Java/EntradaDeDadosFloat.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class EntradaDeDadosFloat {
+	
+	public static void main (String [] args) {
+				
+	Scanner sc = new Scanner (System.in);
+				
+				
+		double x;
+		x = sc.nextDouble();
+		System.out.println("Voce digitou: " + x);
+				
+			
+
+			
+		sc.close();
+}
+			
+
+
+}
---
+++ exercicios-basicos/Teste de processamentos Java/TesteTresProcessamento.java
@@ -0,0 +1,21 @@
+
+public class TesteTresProcessamento {
+
+	public static void main (String [] args) {
+	 
+	
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		resultado = (double) a / b; //o double no começo é pra mostrar numero quebrado
+		//sem o double no começo ele aparece somente o 2.0 e não 2.5
+		
+		System.out.println(resultado);
+
+}
+
+	
+	
+}
